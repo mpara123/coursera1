60a90c70de4c420069627d2c87beb1b4021bf913
mpara123 <mahparaakhatar70@gmail.com>
2025-09-16 16:18:31 +0500

    first commit

---
+++ Untitled-1js.js
@@ -0,0 +1,49 @@
+
+     const employees = [
+  { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript' },
+  { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Recruitment' },
+  { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Accounts' },
+  { id: 4, name: 'Emily Davis', age: 26, department: 'IT', salary: 55000, specialization: 'JavaScript' },
+];
+
+    // Function to display all employees
+function displayEmployees() {
+    const totalEmployees = employees
+        .map(employee => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`)
+        .join('');
+    document.getElementById('employeesDetails').innerHTML = totalEmployees;
+}
+function calculateTotalSalaries() {
+      const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
+      alert(`Total Salaries: $${totalSalaries}`);
+    }
+    
+    function displayHREmployees() {
+     const hrEmployees = employees.filter(employee => employee.department === 'HR');
+      const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+      document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
+}
+function findEmployeeById(employeeId) {
+      const foundEmployee = employees.find(employee => employee.id === employeeId);
+      if (foundEmployee) {
+      document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+      }
+      else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+       }
+   }
+
+function displayJavaScriptEmployees() {
+  const jsEmployees = employees.filter(employee => employee.specialization === 'JavaScript');
+  
+  if (jsEmployees.length > 0) {
+    const jsEmployeesDisplay = jsEmployees
+      .map(employee => `<p>${employee.id}: ${employee.name} - ${employee.department} - ${employee.specialization} - $${employee.salary}</p>`)
+      .join('');
+      
+    document.getElementById('employeesDetails').innerHTML = jsEmployeesDisplay;
+  } else {
+    document.getElementById('employeesDetails').innerHTML = 'No employees found with JavaScript specialization.';
+  }
+}
+  
